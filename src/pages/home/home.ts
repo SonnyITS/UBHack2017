ed2f5d0c04c6eb85dc1db2a431a35f721442217d
WestStreetVariety <37722758+WestStreetVariety@users.noreply.github.com>
2018-03-24 07:35:53 -0400

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,1 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { User } from "../../shared/user";
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+  
+  user = {} as User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AngularFireAuth) {
+  }
+
+  async login(user: User){
+    try{
+      const result = await this.auth.auth.signInWithEmailAndPassword(user.email, user.password);
+      if (result){
+        this.navCtrl.push("ProfilePage");
+      }
+      else{
+        
+      }
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  goToRegister(){
+    this.navCtrl.push('RegistrationPage');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProfilePage');
+  }
+
+}
 
